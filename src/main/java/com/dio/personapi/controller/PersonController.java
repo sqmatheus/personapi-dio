4b01a89bea4103d09ffa230d43b766ca6f1c27ff
Matheus <62452028+sqmatheus@users.noreply.github.com>
2021-07-06 16:01:40 -0300

    update person controller

--- src/main/java/com/dio/personapi/controller/PersonController.java
+++ src/main/java/com/dio/personapi/controller/PersonController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/people")
+@CrossOrigin("*")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
